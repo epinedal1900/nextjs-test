e50e7797c1d2ec072fabc99c5d2d7f6c4590df18
Emmanuel <emanems2019@gmail.com>
2024-04-11 07:55:44 -0600

    dto

---
+++ app/api/_dto/mutationResponse.ts
@@ -0,0 +1,5 @@
+
+export interface MutationResponse {
+    success: boolean
+    message: string
+}
--- app/api/consumer-orders/route.ts
+++ app/api/consumer-orders/route.ts
@@ -5,5 +5,8 @@ type Params = {
 export async function GET(request: Request, context: { params: Params }) {
   const team = context.params.team // '1'
 }
+export async function DELETE(request: Request, context: { params: Params }) {
+  const team = context.params.team // '1'
+}
  
 // Define params type according to your route parameters (see table below)
